337206f237de2cb3e482492e91208332b7137cc9
el hihi zakarya <zakarya.elhihi@gmail.com>
2023-11-30 14:43:54 -0800

    task16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,4 @@
 #include "binary_trees.h"
-#include "binary_trees.h"
-#include "binary_trees.h"
 /**
 * binary_tree_is_leaf - test  if  a  node is a leaf
 * @node: pointer to the node
@@ -15,35 +13,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	return (0);
 }
 /**
-* binary_tree_leaves - counts the leaves of a tree
-* @tree: pointer to a tree
-* Return: count
-*/
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	if (binary_tree_is_leaf(tree))
-	{
-		return (1);
-	}
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-}
-/**
- * binary_tree_is_full - check if tree is full
- * @tree: pointer to the node
- * Return: int
-*/
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-    if (tree == NULL)
-        return (0);
-    if (binary_tree_leaves(tree) % 2 == 0)
-        return (1);
-    else
-        return (0);
-}
-/**
  * binary_tree_is_perfect - check if a tree is perfect
  * @tree: pointer to a tree
  * Return: int
@@ -52,11 +21,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return (0);
+    if (binary_tree_balance(tree) != 0)
+        return (0);
     if (binary_tree_is_leaf(tree))
         return (1);
     if (binary_tree_is_full(tree))
-        return (1);
-    else
-        return (0);
-    return (binary_tree_is_perfect(tree->left) * binary_tree_is_perfect(tree->right));  
+    	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
