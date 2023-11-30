010223ea6629ebcac3947245691f3d2001b9e984
el hihi zakarya <zakarya.elhihi@gmail.com>
2023-11-30 14:20:32 -0800

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -25,7 +25,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_is_leaf(tree))
 		return (0);
-	height_left = binary_tree_height(tree->left) + 1;
-	height_right = binary_tree_height(tree->right) + 1;
+	height_left = tree->left ? binary_tree_height(tree->left) + 1 : 1;
+	height_right = tree->right ? binary_tree_height(tree->right) + 1 : 1;
 	return ((height_left > height_right) ? height_left : height_right);
 }
